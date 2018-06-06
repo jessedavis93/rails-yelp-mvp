826a60f440a404ffbbbac3af8f37f3d7818d1a6e
Jesse Davis <jdavis93@live.com>
2018-06-06 12:38:50 +0200

    Seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Restaurant.create(name: "Ben's", address: "P. Sherman 42 Wallaby Way", phone_number: "8675309", category: "italian")
+Restaurant.create(name: "Hometown", address: "Nowhere", phone_number: "1275309", category: "belgian")
+Restaurant.create(name: "Nickels", address: "The Bank", phone_number: "4678309", category: "french")
+Restaurant.create(name: "Ice Cream", address: "Ice cream road", phone_number: "1275309", category: "japanese")
+Restaurant.create(name: "Asian Buffet", address: "chinatown", phone_number: "8675644", category: "chinese")
